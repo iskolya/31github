cef6b9fcb1efbc192dff95c32a2e871cff760ace
Islam Momukeev <islambek.momukeev_2026@ucentralasia.org>
2023-12-01 23:38:16 +0600

    initialization the project

---
+++ src/main/java/com/example/medcheck/MedCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.medcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedCheckApplication.class, args);
+    }
+
+}
