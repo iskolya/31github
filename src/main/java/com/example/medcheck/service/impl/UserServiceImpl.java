2bb5e90327214bcdd9b6a97101a5869525e1ca5a
Islam Momukeev <islambek.momukeev_2026@ucentralasia.org>
2023-12-11 23:13:53 +0600

    A

--- src/main/java/com/example/medcheck/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/medcheck/service/impl/UserServiceImpl.java
@@ -6,14 +6,22 @@ import com.example.medcheck.service.UserService;
 import com.example.medcheck.share.dto.UserDto;
 import com.example.medcheck.ui.model.request.UserDetailRequestModel;
 import com.example.medcheck.ui.model.response.UserRest;
+import org.antlr.v4.runtime.misc.Utils;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
     UserRepository userRepository;
+
+    Utils utils;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
     @Override
     public UserDto createUser(UserDto userDto) {
         UserEntity storedUserDetails = userRepository.findByEmail(userDto.getEmail());
